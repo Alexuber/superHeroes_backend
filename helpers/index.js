6fe2b3b114352c3f0a388064fc3680f3029a77e2
Alexuber <alexuberbuber@gmail.com>
2023-09-24 16:22:47 +0300

    fixed imports

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,11 @@
 const HttpError = require("./HttpError");
 const handleMongooseError = require("./handleMongooseError");
+const imageHelper = require("./imageHelper");
+const processHeroWithImages = require("./processHeroWithImages");
 
 module.exports = {
   HttpError,
   handleMongooseError,
+  imageHelper,
+  processHeroWithImages,
 };
